4101b650bdaa959cacbcda9ffc20263a559f178a
Paden, Jamal R <jpaden@gatech.edu>
2026-06-18 19:58:58 -0400

    Initial Commit. Got basic application running.

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-// const { app, BrowserWindow } = require('electron')
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import url from 'url';
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+    },
   })
 
-  win.loadFile('index.html')
+  win.loadURL(
+        url.format({
+            pathname: path.join(import.meta.dirname, `./dist/paralegal-sidekick/browser/index.html`),
+            protocol: "file:",
+            slashes: true,
+        })
+  );
+
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.on("ready", createWindow);
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("activate", function () {
+    if (mainWindow === null) createWindow();
+});
