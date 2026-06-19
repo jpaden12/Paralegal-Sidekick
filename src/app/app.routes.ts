b2ecad4dea23f9d12b49a87083e8771f47e7d49f
Paden, Jamal R <jpaden@gatech.edu>
2026-06-19 16:26:57 -0400

    Made progress on Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { FormCompletionComponent } from './features/form-completion/form-completion.component';
+import { PreviewPageComponent } from './features/preview-page/preview-page.component';
+import { ClientProfileComponent } from './features/client-profile/client-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: `Paralegal's Sidekick`
+  },
+  {
+    path: 'form',
+    component: FormCompletionComponent,
+    title: 'Form Completion'
+  },
+  {
+    path: 'form-preview',
+    component: PreviewPageComponent,
+    title: 'Form Preview'
+  },
+  {
+    path: 'profile',
+    component: ClientProfileComponent,
+    title: 'Profile Page',
+  }
+];
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,58 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from "@angular/router";
+
+interface ClientProfile {
+  name: string;
+  date_of_birth: Date; 
+  home_address: string;
+  medical_provider: string;
+}
+
+interface Form {
+  name: string; 
+  state: string;
+  number: number;
+}
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatGridListModule, MatButtonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  clients: ClientProfile[] = [
+    {
+      name: "John Stamos",
+      date_of_birth: new Date(1996, 4, 4), 
+      home_address: "123 Sesame St", 
+      medical_provider: "Sanjay Gupta"
+    },
+    {
+      name: "SpongeBob SquarePants",
+      date_of_birth: new Date(1999, 3, 4), 
+      home_address: "123 Sesame St", 
+      medical_provider: "Dr. Krabs"
+    },
+    {
+      name: "Jalen Brunson",
+      date_of_birth: new Date(1990, 5, 4), 
+      home_address: "Madison Sq Garden", 
+      medical_provider: "Sanjay Gupta"
+    }
+  ]
+
+  forms: Form[] = [
+    {
+      name: "HIPAA Authorization",
+      state: "NY",
+      number: 960
+    }
+  ]
+
+}
