26af5fd2644092e2914381cfc79c8de3418036e0
Paden, Jamal R <jpaden@gatech.edu>
2026-07-21 16:50:42 -0400

    Removing local DB service files.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,16 +6,15 @@ import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { Router, RouterLink } from "@angular/router";
 import { AppService } from '../../app.service';
 import { FormControl, FormsModule, ReactiveFormsModule, FormGroup, ɵInternalFormsSharedModule } from '@angular/forms';
-import { LocalDbService } from '../../core/local-db.service';
 import { ClientProfile } from '../../models/client-profile.model';
 import { FormFields } from '../../models/form-fields.model';
 import { FormFieldsService } from '../../core/form-fields.service';
-import { form } from '@angular/forms/signals';
+import { ClientProfileService } from '../../core/client-profile.service';
 
 
 @Component({
   selector: 'home',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatGridListModule, MatButtonModule, RouterLink, ɵInternalFormsSharedModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatGridListModule, MatButtonModule, ɵInternalFormsSharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -24,22 +23,22 @@ export class HomeComponent implements OnInit {
 
   protected globalState: AppService = inject(AppService);
   protected router: Router = inject(Router);
-  protected dbService: LocalDbService = inject(LocalDbService);
   protected formFieldsService: FormFieldsService = inject(FormFieldsService);
+  protected clientProfileService: ClientProfileService = inject(ClientProfileService);
 
   protected returnedClients: ClientProfile[] | undefined;
 
   protected _forms: Record<string, FormFields>;
   protected forms: FormFields[] = [];
-  
+
 
   clients: ClientProfile[] = [
     {
       id: 1,
-      name: "Khloe Kardashian", 
+      name: "Khloe Kardashian",
       date_of_birth: new Date(),
       ssn: 1,
-      address: "string", 
+      address: "string",
 
       medical_provider_name: "The Doctor",
       medical_provider_address: "123 Medical Street",
@@ -47,10 +46,10 @@ export class HomeComponent implements OnInit {
     },
     {
       id: 2,
-      name: "Patrick Mahomes", 
+      name: "Patrick Mahomes",
       date_of_birth: new Date(),
       ssn: 2,
-      address: "string", 
+      address: "string",
 
       medical_provider_name: "The Doctor",
       medical_provider_address: "123 Physicans Drive",
@@ -58,10 +57,10 @@ export class HomeComponent implements OnInit {
     },
     {
       id: 3,
-      name: "Jerry Seinfeld", 
+      name: "Jerry Seinfeld",
       date_of_birth: new Date(),
       ssn: 3,
-      address: "string", 
+      address: "string",
 
       medical_provider_name: "The Doctor",
       medical_provider_address: "123 Nursing Avenue",
@@ -69,10 +68,10 @@ export class HomeComponent implements OnInit {
     },
     {
       id: 4,
-      name: "Lin-Manuel Miranda", 
+      name: "Lin-Manuel Miranda",
       date_of_birth: new Date(),
       ssn: 4,
-      address: "string", 
+      address: "string",
 
       medical_provider_name: "The Doctor",
       medical_provider_address: "123 Feelbetter Blvd",
@@ -84,24 +83,24 @@ export class HomeComponent implements OnInit {
     clientSelect: new FormControl(''),
     formSelect: new FormControl(''),
   });
-  
+
   ngOnInit(): void {
-    
+
   }
 
   constructor() {
     // Replace with data persistence
-    this.dbService.createNewClient(this.clients[0]);
-    this.dbService.createNewClient(this.clients[1]);
-    this.dbService.createNewClient(this.clients[2]);
-    this.dbService.createNewClient(this.clients[3]);
+    this.clientProfileService.createNewClient(this.clients[0]);
+    this.clientProfileService.createNewClient(this.clients[1]);
+    this.clientProfileService.createNewClient(this.clients[2]);
+    this.clientProfileService.createNewClient(this.clients[3]);
 
     this._forms = this.formFieldsService.formMap;
     this.forms[0] = this._forms["HIPAA Authorization"];
     this.forms[1] = this._forms["Court Subpoena"];
 
     effect((onCleanup) => {
-      const clientsCursor = this.dbService.getAllClients();
+      const clientsCursor = this.clientProfileService.getAllClients();
       this.returnedClients = clientsCursor?.fetch();
 
       onCleanup(() => {
@@ -119,14 +118,14 @@ export class HomeComponent implements OnInit {
   }
 
   proceedToForm() {
-    // Check if profile and form fields are filled out. Illuminate forms if 
-    // they aren't. 
+    // Check if profile and form fields are filled out. Illuminate forms if
+    // they aren't.
     const selectedClient: string = this.clientAndFormSelect.get('clientSelect')?.getRawValue();
     const selectedForm: string = this.clientAndFormSelect.get('formSelect')?.getRawValue();
-    
+
     // Update client state
     if (this.clientAndFormSelect.get('clientSelect')?.value == "") {
-      
+
       // Show an error message
     } else {
       // Update the global state with the selected client
@@ -138,7 +137,7 @@ export class HomeComponent implements OnInit {
 
     // Update form state
     if (this.clientAndFormSelect.get('formSelect')?.getRawValue() == "") {
-      
+
     } else {
       const formToSwitchTo: FormFields = this.formFieldsService.formMap[selectedForm];
       this.globalState.setCurrentForm(formToSwitchTo);
@@ -153,7 +152,7 @@ export class HomeComponent implements OnInit {
     const selectedForm: string = this.clientAndFormSelect.get('formSelect')?.getRawValue();
 
     if (this.clientAndFormSelect.get('clientSelect')?.value == "") {
-      
+
       // Show an error message
     } else {
       // Update the global state with the selected client
@@ -162,16 +161,16 @@ export class HomeComponent implements OnInit {
         this.globalState.setCurrentClientProfile(switchToClient);
       }
     }
-    
+
       // Update form state
     if (this.clientAndFormSelect.get('formSelect')?.getRawValue() == "") {
-      
+
     } else {
       const formToSwitchTo: FormFields = this.formFieldsService.formMap[selectedForm];
       this.globalState.setCurrentForm(formToSwitchTo);
     }
-    
-    
+
+
     this.router.navigate(['/preview'])
   }
 
